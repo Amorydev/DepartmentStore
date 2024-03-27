c164d466af4c7aae5a09e3a3277ab7b569c791a5
Amory.dev <amory.dev@gmail.com>
2024-03-27 23:08:28 +0700

    Lasted Update 27/03/2024

--- app/src/main/java/com/amory/departmentstore/MainActivity.kt
+++ app/src/main/java/com/amory/departmentstore/MainActivity.kt
@@ -2,6 +2,8 @@ package com.amory.departmentstore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import com.amory.departmentstore.databinding.ActivityMainBinding
 
@@ -30,15 +32,18 @@ class MainActivity : AppCompatActivity() {
         imageView4.setImageResource(R.drawable.discount_2)
         imageView4.scaleType = ImageView.ScaleType.FIT_XY
 
-
         binding.viewFlipper.addView(imageView1)
         binding.viewFlipper.addView(imageView2)
         binding.viewFlipper.addView(imageView3)
         binding.viewFlipper.addView(imageView4)
 
+        val slide_in = AnimationUtils.loadAnimation(applicationContext,R.anim.slide_in_right)
+        val slide_out = AnimationUtils.loadAnimation(applicationContext,R.anim.slide_out_right)
         /*Chuyển đổi ảnh*/
-        binding.viewFlipper.flipInterval = 1000
+        binding.viewFlipper.flipInterval = 4000
         binding.viewFlipper.isAutoStart = true
+        binding.viewFlipper.inAnimation = slide_in
+        binding.viewFlipper.inAnimation = slide_out
 
     }
 }
