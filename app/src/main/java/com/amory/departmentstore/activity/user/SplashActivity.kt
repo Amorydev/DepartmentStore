c620283dea57684fc37d4a014dc100e47a40f3ec
Amory <amory.dev@gmail.com>
2024-09-25 15:28:30 +0700

    fix: Replace Handler by kotlin coroutines

--- app/src/main/java/com/amory/departmentstore/activity/user/SplashActivity.kt
+++ app/src/main/java/com/amory/departmentstore/activity/user/SplashActivity.kt
@@ -4,27 +4,36 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 import com.amory.departmentstore.R
 import com.amory.departmentstore.Utils.Utils
 import com.amory.departmentstore.activity.MainActivity
 import com.amory.departmentstore.model.User
 import io.paperdb.Paper
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
+@DelicateCoroutinesApi
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         Paper.init(this)
-        Handler().postDelayed({
-            val user = Paper.book().read<User>("user")
-            if (user != null ) {
-                Utils.user_current = user
-            }
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
-        }, 1500)
+        GlobalScope.launch {
+            delay(1500)
+            checkUser()
+        }
+    }
+
+    private fun checkUser() {
+        val user = Paper.book().read<User>("user")
+        if (user != null ) {
+            Utils.user_current = user
+        }
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
